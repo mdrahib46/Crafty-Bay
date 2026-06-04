a222ac15ca6fb9186b5a6b2ce08f37061543799a
mdrahib46 <nahid179846@bsdi-bd.org>
2026-06-04 13:06:15 +0600

    Feat : Firebase Crashlytics & Analytics has been added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'dart:ui';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'my_app.dart';
-import 'firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 // Business Layer Q&A
 // Scope of work - Crafty Bay App
@@ -18,6 +18,15 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const MyApp());
 }
 
