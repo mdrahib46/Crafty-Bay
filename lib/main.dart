ed69da0e947fca4309186ec96e8b001a679c4fd5
mdrahib46 <nahid179846@bsdi-bd.org>
2026-06-04 12:59:59 +0600

    Feat : Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'my_app.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+// Business Layer Q&A
+// Scope of work - Crafty Bay App
+// Design UI/UX -> Q&A
+// Project Initiate -> Define project version
+
+
+// --------- Coding Part---------//
+// Setup Firebase
+// Crashlytics
+// Project Structure -> MVVM , Clear Architecture, MVP, Onion
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
